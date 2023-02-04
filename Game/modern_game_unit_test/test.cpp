1a31ca65122a623eaf5b385801514b26048c8f93
matkad92 <mateuszkadziolka92@gmail.com>
2023-02-04 20:07:16 +0100

    Adding baisic window and ball UT

--- Game/modern_game_unit_test/test.cpp
+++ Game/modern_game_unit_test/test.cpp
@@ -1,7 +1,17 @@
 #include "pch.h"
+#include "../modern_cpp_game/constants.h"
+
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
 
+TEST(GameWindowSize, GameWindowSizeGreaterThanZeroPix) {
+	EXPECT_GT(constants::window_height, 0);
+	EXPECT_GT(constants::window_width, 0);
+}
+
+TEST(BallVelocity, BallVelocGreaterThanZero) {
+	EXPECT_GT(constants::ballSpeed, 0);	
+}
